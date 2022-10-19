06693a2c6af48a9d9de8c95510b25b19b1abfa14
qxan <mehmet.enfa@gmail.com>
2022-10-19 16:38:20 +0300

    task

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Layout } from "./components"
 
 function App() {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center bg-orange-300">
-      sdfgnsafgnsfgns
+    <div className="w-full min-h-screen flex justify-center items-start bg-orange-300">
+      <Layout />
     </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Header, AddTask, Task } from ".";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+  const [isActive, setIsActive] = useState(false);
+  const [data, setData] = useState([
+    {
+      id: new Date().getMilliseconds(),
+      title: "The Alpha Team",
+      date: "Dec 12 12.00PM",
+      complated: false,
+    },
+  ]);
 
-export default Layout
+  // Func
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date().getMilliseconds(),
+      title: e.target.taskName.value,
+      date: e.target.selectt.value,
+      complate: false,
+    };
+    setData([...data, newTodo])
+  };
+  // Func Section is Done
+  return (
+    <div className="mt-[2.5rem] w-[40rem] bg-pink-500 pb-10">
+      <Header active={isActive} setActive={setIsActive} />
+      {isActive && (
+        <div className="w-[30rem] mx-auto ">
+          <AddTask handleSubmit={handleSubmit} />
+        </div>
+      )}
+      {data.length !== 0 ? (
+        <div>
+            {data.map((item, idx) => (
+                <div key={idx}>
+                    <Task data={item} />
+                </div>
+            ))}
+        </div>
+      ) : (
+        <p className="text-center text-xl font-semibold">No Task Yet</p>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
